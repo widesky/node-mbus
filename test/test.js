e029b5e95ca00b0a0ce912a025dba47af0994e18
Ingo Fischer <github@fischer-ka.de>
2018-02-23 11:48:23 +0100

    change test

--- test/test.js
+++ test/test.js
@@ -1,10 +1,35 @@
 var net    = require('net');
 
-var connected   = false;
-var tcpClient   = new net.Socket();
-var lastMessage;
 var port        = 15000;
+var lastMessage;
 
+
+var server = net.createServer(function(socket) {
+    console.log('Connected!');
+
+    socket.on('data', function (data) {
+        if (!data) {
+            console.log('Received empty string!');
+            return;
+        }
+        console.log('Received ' + data);
+        lastMessage = data;
+
+        var sendBuf = Buffer.from('683C3C680808727803491177040E16290000000C7878034911041331D40000426C0000441300000000046D1D0D98110227000009FD0E0209FD0F060F00008F13E816', 'hex');
+        sendMessage(sendBuf);
+    });
+    socket.on('error', function (err) {
+        console.error('Error: ' + err);
+    });
+    socket.on('close', function () {
+        console.error('Close');
+    });
+    socket.on('end', function () {
+        console.error('End');
+    });
+});
+
+server.listen(port, '127.0.0.1');
 
 
 function sendMessage(message, callback) {
@@ -13,23 +38,6 @@ function sendMessage(message, callback) {
     });
 }
 
-tcpClient.on('data', function (data) {
-    if (!data) {
-        console.log('Received empty string!');
-        return;
-    }
-    console.log('Received ' + data);
-    lastMessage = data;
-
-    var sendBuf = Buffer.from('683C3C680808727803491177040E16290000000C7878034911041331D40000426C0000441300000000046D1D0D98110227000009FD0E0209FD0F060F00008F13E816', 'hex');
-    sendMessage(sendBuf);
-});
-tcpClient.on('error', function (err) {
-    console.error('Error: ' + err);
-});
-tcpClient.connect(port, '127.0.0.1', function() {
-    console.log('Connected!!');
-});
 
 
 
