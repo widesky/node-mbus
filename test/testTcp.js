b3f1540fe5895fbc90c5aefb05b9661eb84b2485
Ingo Fischer <github@fischer-ka.de>
2018-03-12 09:51:21 +0100

    fix testing

--- test/testTcp.js
+++ test/testTcp.js
@@ -110,14 +110,14 @@ describe('Native libmbus node-module TCP test ...', function() {
                     console.log(new Date().toString() + ': mbus-TCP-Master data: ' + JSON.stringify(data, null, 2));
                     expect(err).to.be.null;
                     expect(data.SlaveInformation.Id).to.be.equal(11490378);
-                    expect(data.DataRecord[0].Value).to.be.equal('11490378');
+                    expect(data.DataRecord[0].Value).to.be.equal(11490378);
 
                     mbusMaster.getData(2, function(err, data) {
                         console.log(new Date().toString() + ': mbus-TCP-Master err: ' + err);
                         console.log(new Date().toString() + ': mbus-TCP-Master data: ' + JSON.stringify(data, null, 2));
                         expect(err).to.be.null;
                         expect(data.SlaveInformation.Id).to.be.equal(5000244);
-                        expect(data.DataRecords[0].Value).to.be.equal('1252');
+                        expect(data.DataRecord[0].Value).to.be.equal(1252);
 
                         mbusMaster.scanSecondary(function(err, data) {
                             console.log(new Date().toString() + ': mbus-TCP-Master err: ' + err);
