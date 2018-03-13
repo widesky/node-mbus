af7e1721897b25f0afc6238707cbc6073e2cdf45
Ingo Fischer <github@fischer-ka.de>
2018-03-13 22:35:58 +0100

    another try

--- test/testSerial.js
+++ test/testSerial.js
@@ -109,7 +109,7 @@ describe('Native libmbus node-module Serial test ...', function() {
                 socat = spawn('socat', ['-Dxs', 'pty,link=/tmp/virtualcom0,ispeed=9600,ospeed=9600,raw', 'tcp:127.0.0.1:15001']);
             }
             else { // for manual tests use com0com to create a virtual COM pair and com2tcp to direct one side to tcp
-                socat = spawn(process.env.APPVEYOR_BUILD_FOLDER + '\\com2tcp.exe', ['--ignore-dsr', '--baud', '9600', '--parity', 'e', '\\.\\CNCA0', '127.0.0.1', '15001']);
+                socat = spawn(process.env.APPVEYOR_BUILD_FOLDER + '\\com2tcp.exe', ['--ignore-dsr', '--baud', '9600', '--parity', 'e', '\\\\.\\CNCA0', '127.0.0.1', '15001']);
             }
             console.log('mbus-Serial-Device: Socat spawned');
             socat.stdout.on('data', function(data) {
