224c524c51189d641359bf2db4fe2bb93e2e49fc
Ingo Fischer <github@fischer-ka.de>
2019-05-04 18:13:37 +0200

    try change is to new style

--- src/mbus-master.cc
+++ src/mbus-master.cc
@@ -55,7 +55,7 @@ NAN_MODULE_INIT(MbusMaster::Init) {
 
     v8::Local<v8::Function> function = Nan::GetFunction(tpl).ToLocalChecked();
     constructor.Reset(function);
-    NaN:Set(target, Nan::New<v8::String>("MbusMaster").ToLocalChecked(), function);
+    target->Set(Nan::New("MbusMaster").ToLocalChecked(), function);
 }
 
 NAN_METHOD(MbusMaster::New) {
